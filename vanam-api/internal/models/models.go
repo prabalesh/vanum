2685993a59eb16d77bbe7be63280b41265694c28
Prabalesh <prabalesh2008@gmail.com>
2025-09-07 17:27:38 +0530

    feat(vanam-api): add user and role model

---
+++ vanam-api/internal/models/models.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Role struct {
+	ID   uint   `gorm:"primaryKey"`
+	Name string `gorm:"unique;not null"`
+}
+
+type User struct {
+	ID        uint   `gorm:"primaryKey"`
+	Email     string `gorm:"unique;not null"`
+	Password  string `gorm:"not null"`
+	Name      string `gorm:"not null"`
+	RoleID    uint   `gorm:"not null"`
+	Role      Role   `gorm:"foreignKey:RoleID"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
