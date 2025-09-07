da4020212766c4e6e5efddab06cd93f8a0c073ac
Prabalesh <prabalesh2008@gmail.com>
2025-09-07 16:55:59 +0530

    feat(vanam-api): adding cors middleware

---
+++ vanam-api/internal/middleware/cors.go
@@ -0,0 +1,29 @@
+package middleware
+
+import "github.com/gin-gonic/gin"
+
+func CORS() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		origin := c.Request.Header.Get("Origin")
+
+		allowedOrigins := map[string]bool{
+			"http://localhost:3000": true, // User app
+			"http://localhost:3001": true, // Admin app
+		}
+
+		if allowedOrigins[origin] {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
